674d825b80de4e700abf56a76b521ff40189a7a1
0336-Talha <0336-Talha~>
2025-01-13 16:52:41 +0500

    aik choti c change

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/account';
 
     /**
      * Create a new controller instance.
@@ -72,5 +72,6 @@ class RegisterController extends Controller
 
             'password' => Hash::make($data['password']),
         ]);
+
     }
 }
